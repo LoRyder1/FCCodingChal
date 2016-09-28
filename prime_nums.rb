c675bbf3e2d75ff8d4709e890c64640023158a22
Lovinder Pnag <lspnag@gmail.com>
2016-09-27 19:39:12 -0700

    use a while loop

--- prime_nums.rb
+++ prime_nums.rb
@@ -6,21 +6,29 @@ Write a program that prints out a multiplication table of the first 10 prime num
 - The first row and column of the table should have the 10 primes, with each cell containing the product of the primes for the corresponding row and column. 
 OBJECTIVE
 
+# Object Oriented Programming - sending a message to methods
 
 class Fixnum
   def is_prime?
-    true
+    x = self - 1
+    while x > 1
+      return false if (self % x).zero?
+      x -=1
+    end
+    true unless self == 1
   end
 end
 
-def prime_numbers
-  nums = (1..10).to_a
+def prime_numbers n
+  count = 1
   primes = []
-  nums.each do |x|
-    primes << x if x.is_prime?
+  while primes.size != 10
+    primes << count if count.is_prime?
+    count += 1
   end
+
   primes
 end
 
 
-p prime_numbers
+p prime_numbers 10
