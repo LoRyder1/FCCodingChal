538161e9891ac7560b6c3dc3adc82da6ecc73917
Lovinder Pnag <lspnag@gmail.com>
2016-09-29 09:03:28 -0700

    tidy up

--- lib/prime_nums.rb
+++ lib/prime_nums.rb
@@ -1,13 +1,5 @@
-# Funding Circle Coding Challenge
 require 'pp'
-require 'benchmark'
-
-<<-OBJECTIVE
-Write a program that prints out a multiplication table of the first 10 prime numbers.
-- The program must run from the command line and print one table to STDOUT.
-- The first row and column of the table should have the 10 primes, with each cell containing the product of the primes for the corresponding row and column. 
-OBJECTIVE
-
+require 'pry'
 # Object Oriented Programming - sending a message to methods
 # 1.is_prime? == nil
 
@@ -18,90 +10,94 @@ class Fixnum
       return false if (self % x).zero?
       x -=1
     end
-    # 1 is not a prime number
-    true unless self == 1
+    true unless self == 1 # 1 is not a prime number
   end
 end
 
 class TableOfPrimes
-  attr_reader :primes
-  def initialize
-    @primes = []
-    @matrix = []
+  attr_reader :primes, :matrix, :length
+  def initialize length
+    @primes, @matrix, @length = [], [], length
+    prime_numbers
   end
 
-  def prime_numbers n
-    @primes, @matrix = [], [] 
+  def prime_numbers
     count = 1
-    while primes.size != n
-      @primes << count if count.is_prime?
+    while primes.size != length
+      primes << count if count.is_prime?
       count += 1
     end
-    @matrix << primes
+    matrix << primes
   end
 
   def create_table
     print '     '
-    @primes.each{|i| print "%-3d  " % i}
+    primes.each{|i| print "%-3d  " % i}
     print "\n     "
-    @primes.each{|i| print '---- '}
+    primes.each{|i| print '---- '}
     print "\n"
 
-    @primes.each do |j|
+    primes.each do |j|
       print "%-3d| " % j
-      @primes.each{|i| print "%-3d  " % (i*j)}
+      primes.each{|i| print "%-3d  " % (i*j)}
       print "\n"
     end
   end
 
   def create_matrix
-    @primes.each do |j|
+    primes.each do |j|
       arr = [j]
-      @primes.each{|i| arr << (i*j)}
-      @matrix << arr
+      primes.each{|i| arr << (i*j)}
+      matrix << arr
     end
-    @matrix[0].unshift('')
-    @matrix
+    matrix[0].unshift('')
   end
 
   def functional_programming
     count = 0
-    @primes.product(@primes).map{|x,y| x*y}.each_slice(10) do |x|
-      @matrix << x.to_a.unshift(@primes[count])
+    primes.product(primes).map{|x,y| x*y}.each_slice(length) do |x|
+      matrix << x.to_a.unshift(primes[count])
       count+=1
     end
-    @matrix[0].unshift('')
-    @matrix
+    matrix[0].unshift('')
   end
 
 end
 
 # ======= CONTROLLER =========
 
-  print "\e[2J"
-  
-puts 'Welcome to Prime Number Multiplication Tables'
-  print "====================================================\n"
-  pr = TableOfPrimes.new
-  pr.prime_numbers 10
-  puts "Table with the first 10 prime numbers:"
+puts <<-WELCOME
+\e[2J
+Welcome to Prime Number Multiplication Tables
+====================================================
+
+WELCOME
+ 
+  pr = TableOfPrimes.new(10)
+
+puts <<-TABLE1
+Table with the first 10 prime numbers:
+TABLE1
+
   pr.create_table
-  print "====================================================\n\n"
-  
-puts "What N size prime number multiplication would you like?\n"
+
+puts <<-QUESTION
+
+====================================================
+
+What N size prime number multiplication would you like?
+QUESTION
+
   n = gets.chomp
-  print "\n"
 
-puts 'Table with the first ' + n + ' prime numbers:'
-  print "====================================================\n"
+puts <<-TABLE2
 
-  pr.prime_numbers(n.to_i)
-  pr.create_table
+====================================================
 
-  print "\n"
+Table with the first #{n} prime numbers:
+TABLE2
 
-
-
-
+  pr2 = TableOfPrimes.new(n.to_i) 
+  pr2.create_table
 
 
--- spec/prime_nums_spec.rb
+++ spec/prime_nums_spec.rb
@@ -31,6 +31,21 @@ describe 'TableOfPrimes' do
       print_output ="     2    3    5    7    \n     ---- ---- ---- ---- \n2  | 4    6    10   14   \n3  | 6    9    15   21   \n5  | 10   15   25   35   \n7  | 14   21   35   49   \n"
       expect{subject.create_table}.to output(print_output).to_stdout
     end
+  end
 
+  describe '#create_matrix' do
+    it 'creates a 2D matrix multiplication table of primes' do
+      subject.prime_numbers(4)
+      subject.create_matrix
+      expect(subject.matrix).to eq [["", 2, 3, 5, 7], [2, 4, 6, 10, 14], [3, 6, 9, 15, 21], [5, 10, 15, 25, 35], [7, 14, 21, 35, 49]]
+    end
+  end
+
+  describe '#functional_programming' do
+    it 'creates a 2D matrix multiplication table of primes' do
+      subject.prime_numbers(4)
+      subject.functional_programming(4)
+      expect(subject.matrix).to eq [["", 2, 3, 5, 7], [2, 4, 6, 10, 14], [3, 6, 9, 15, 21], [5, 10, 15, 25, 35], [7, 14, 21, 35, 49]]
+    end
   end
 end
